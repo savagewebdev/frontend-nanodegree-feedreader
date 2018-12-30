f6e6cd986f2c579bac6e810d7b147122a2af3906
Robin Savage <rbnsavage@gmail.com>
2018-12-29 18:14:41 -0600

    Hidden menu test 1.1

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,7 +59,7 @@ $(function() {
          */
 
          it('check to see if the menu is hidden'), function() {
-           expect($('body')).toBeHidden();
+           expect($('body.menu-hidden'')).toBeHidden();
          }
 
        });
