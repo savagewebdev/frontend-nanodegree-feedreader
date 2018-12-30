f9c833706a3f4aeae02f1ff680ffbe7c101e13c4
Robin Savage <rbnsavage@gmail.com>
2018-12-29 19:54:46 -0600

    Quick test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,8 +59,8 @@ $(function() {
          */
 
          it('check to see if the menu is hidden', function() {
-            // expect('click').not.toHaveBeenTriggeredOn($('.menu-icon-link'));
-            expect($('<body class="menu-hidden"></body>')).toHaveClass('menu-hidden');
+            expect($('body.hidden-menu')).toExist();
+            // expect($('<body class="menu-hidden"></body>')).toHaveClass('menu-hidden');
             // expect($('<div class="some-class"></div>')).toHaveClass("some-class")
        });
   });
