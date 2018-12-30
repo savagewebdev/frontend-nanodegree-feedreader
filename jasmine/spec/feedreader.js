90a18854e8e3b6c63a95c6dcb6c72a7043c0d667
Robin Savage <rbnsavage@gmail.com>
2018-12-29 19:29:52 -0600

    Test negation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -60,7 +60,7 @@ $(function() {
 
          it('check to see if the menu is hidden', function() {
             // expect('click').not.toHaveBeenTriggeredOn($('.menu-icon-link'));
-            expect($('body')).toHaveClass('menu-hidden');
+            expect($('body')).not.toHaveClass('menu-hidden');
        });
   });
          /* TODO: Write a test that ensures the menu changes
